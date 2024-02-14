8fcd67a1c9babfdb1eefa9da09b83203c2a85892
Hasaan Ali <hasaan@codesOrbit.com>
2024-02-14 16:47:46 +0500

    enviorment variable added

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '77c32a0ba87d43bb865ed151d2351fe7'
+    key: process.env.REACT_APP_API_KEY
   }
 })
